2587405134258da271fa3026d51207a612f6ee07
Jonathan Cameron <jmcameron@gmail.com>
2015-10-30 22:36:43 -0700

    Initial version. Mostly working

---
+++ loop.py
@@ -0,0 +1,172 @@
+"""
+Implement the basic small loop antenna equations developed by Ted Hart
+"""
+from __future__ import print_function
+
+
+from math import sqrt, pi, log10
+
+# The known shapes
+
+CIRCLE = 'circle'
+OCTAGON = 'octagon'
+
+KNOWN_SHAPES = (CIRCLE, OCTAGON)
+
+
+# Some convenience functions
+
+def kilo(v):
+    return v * 1.0e3
+
+def micro(v):
+    return v * 1.0e6
+
+def pico(v):
+    return v * 1.0e12
+
+
+
+class Loop(object):
+    
+    def __init__(self, shape, s, f, d, p):
+        """
+        @param s : conductor length, feet
+        @param f : operating frequncy, MHz
+        @param d : conductor diameter, inches
+        @param p : transmitter power, watts
+        """
+        if shape not in KNOWN_SHAPES:
+            raise ValueError("ERROR: '%s' is not a know loop shape!" % shape)
+
+        self._shape = shape
+        self._s = s
+        self._f = f
+        self._d = d
+        self._p = p
+
+        self._l = None # Leg length, feet
+        self._diameter = None # Loop diameter, feet
+        self._a = None # area of loop, square feet
+
+        self.recomputeShape()
+
+
+    def recomputeShape(self):
+        if self._shape == CIRCLE:
+            self._l = 0.0
+            self._diameter = self._s / pi
+            self._a = pi * self._diameter**2.0
+            
+        elif self._shape == OCTAGON:
+            self._l = self._s / 8.0
+            self._diameter = self._l * (1.0 + sqrt(2.0))
+            self._a = 2.0 * (1.0 + sqrt(2.0)) * self._l**2.0
+
+
+    # ----------------------------------------------------------------------
+    # Set up properties
+
+    @property
+    def shape(self):
+        return self._shape
+
+    @shape.setter
+    def shape(self, val):
+        self._shape = val
+        self.recomputeShape()
+
+    @property
+    def s(self):
+        return self._s
+
+    @s.setter
+    def s(self, val):
+        self._s = val
+        self.recomputeShape()
+
+    @property
+    def f(self):
+        return self._f
+
+    @f.setter
+    def f(self, val):
+        self._f = val
+
+    @property
+    def d(self):
+        return self._d
+
+    @d.setter
+    def d(self, val):
+        self._d = val
+
+    @property
+    def p(self):
+        return self._p
+
+    @p.setter
+    def p(self, val):
+        self._p = val
+
+    # ----------------------------------------------------------------------
+
+    def Rr(self):
+        """Radiation ressistance, ohms"""
+        return 3.38e-8 * (self._f**2.0 * self._a)**2.0
+
+    def Rl(self):
+        """Loss resistance, ohms)"""
+        return 9.96e-4 * sqrt(self._f) * self._s / self._d
+
+    def L(self):
+        """Inductance, henrys"""
+        return 1.9e-8 * self._s*(7.353 * log10(96*self._s/(pi*self._d)) - 6.386)
+
+    def Xl(self):
+        """Inductive reactance"""
+        return 2.0 * pi * self._f * self.L() * 1.0e6
+
+    def Ct(self):
+        """Tuning capacitor, farads"""
+        return 1.0/(2.0 * pi * self._f * self.Xl() * 1.0e6)
+
+    def Q(self):
+        """Quality factor"""
+        return self.Xl()/(2.0 * (self.Rr()  + self.Rl()))
+
+    def efficiency(self):
+        """Efficiency (0 to 1.0)"""
+        return self.Rr()/(self.Rr() + self.Rl())
+
+    def bandwidth(self):
+        """Loop bandwidth, hertz"""
+        return self._f / self.Q()
+
+    def Cd(self):
+        """Distributed capacity, pF"""
+        return 0.82 * self._s
+
+    def Vc(self):
+        """Capacitor potential, volts"""
+        return sqrt(self._p * self.Xl() * self.Q())
+
+    def summary(self, indent=''):
+        sum = "Loop antenna shape: %s\n" % self._shape
+        sum += indent + "   Conductor length: %.3f feet\n" % self._s
+        sum += indent + "   Conductor diameter: %.3f inches\n" % self._d
+        sum += indent + "   Segment length: %.3f feet\n" % self._l
+        sum += indent + "   Loop area: %.3f square feet\n" % self._a
+        sum += indent + "   Loop diameter: %.3f feet\n" % self._diameter
+        sum += indent + "Operating frequency: %.3f MHz\n" % self._f
+        sum += indent + "Loss resistance: %.4f ohms\n" % self.Rl()
+        sum += indent + "Radiation resistance: %.4f ohms\n" % self.Rr()
+        sum += indent + "Inductance: %.3f henrys\n" % micro(self.L())
+        sum += indent + "Inductive reactance: %.3f ohms\n" % self.Xl()
+        sum += indent + "Tuning capacitance: %.3f pF\n" % pico(self.Ct())
+        sum += indent + "Capacitor voltage: %.1f volts\n" % self.Vc()
+        sum += indent + "Distributed capacity: %.3f pF\n" % self.Cd()
+        sum += indent + "Antenna bandwidth: %.3f kHz\n" % kilo(self.bandwidth())
+        sum += indent + "Quality Factor (Q): %.2f\n" % self.Q()
+        sum += indent + "Efficiency: %.1f %%" % (self.efficiency() * 100.0)
+        return sum
---
+++ loop_test.py
@@ -0,0 +1,62 @@
+"""
+Tests for Loop
+
+>>> from loop import Loop
+
+>>> a = Loop('octagon', 9.0, 14.0, 0.9, 100.0)
+
+>>> print(a.summary())
+Loop antenna shape: octagon
+   Conductor length: 9.000 feet
+   Conductor diameter: 0.900 inches
+   Segment length: 1.125 feet
+   Loop area: 6.111 square feet
+   Loop diameter: 2.716 feet
+Operating frequency: 14.000 MHz
+Loss resistance: 0.0373 ohms
+Radiation resistance: 0.0485 ohms
+Inductance: 2.033 henrys
+Inductive reactance: 178.805 ohms
+Tuning capacitance: 63.579 pF
+Capacitor voltage: 4317.5 volts
+Distributed capacity: 7.380 pF
+Antenna bandwidth: 13.429 kHz
+Quality Factor (Q): 1042.51
+Efficiency: 56.5 %
+
+NOTE: This does not correspond exactly to the loop calculator on 
+      http://www.66pacific.com/calculators/small_tx_loop_calc.aspx
+      Not sure if there is a problem or not.  But it is close.  The
+      differences may be to approximations by Hart for octagons?
+
+
+Now adjust a couple of things
+
+>>> a.s = 12.0
+>>> a.f = 10.0
+
+>>> print(a.summary())
+Loop antenna shape: octagon
+   Conductor length: 12.000 feet
+   Conductor diameter: 0.900 inches
+   Segment length: 1.500 feet
+   Loop area: 10.864 square feet
+   Loop diameter: 3.621 feet
+Operating frequency: 10.000 MHz
+Loss resistance: 0.0420 ohms
+Radiation resistance: 0.0399 ohms
+Inductance: 2.920 henrys
+Inductive reactance: 183.451 ohms
+Tuning capacitance: 86.756 pF
+Capacitor voltage: 4533.1 volts
+Distributed capacity: 9.840 pF
+Antenna bandwidth: 8.927 kHz
+Quality Factor (Q): 1120.14
+Efficiency: 48.7 %
+
+
+"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
